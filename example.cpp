7d227ee7dac1e57a43390b3cbe797d9f33fdd3ce
nblockbuster <34419030+nblockbuster@users.noreply.github.com>
2021-12-24 13:50:33 -0500

    works

--- example.cpp
+++ example.cpp
@@ -1,8 +1,13 @@
-// This example may or may not work, I haven't tested it.
-// There could be some missing imports as well.
-// This also only shows how to parse a vertex buffer.
+#include <string>
+#include <unordered_map>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <stdio.h>
 
-//ease of use cus why not
+// This only shows how to parse a vertex buffer.
+
 void filePutContents(const std::string& name, const std::string& content, bool append = false) {
 	std::ofstream outfile;
 	if (append)
@@ -11,7 +16,6 @@ void filePutContents(const std::string& name, const std::string& content, bool a
 		outfile.open(name);
 	outfile << content;
 }
-//again, ease of use
 
 template <typename T>
 std::string to_str(const T a_value, const int n = 6)
@@ -24,24 +28,25 @@ std::string to_str(const T a_value, const int n = 6)
 
 int main()
 {
-  std::string vbufferpath;
-  std::string outputFile;
-  
-  std::cout << "Vertex Buffer path: ";
-  std::cin >> vbufferpath;
-  std::cout << "Output File Name: ";
-  std::cin >> outputFile;
-  
-  FILE* vbFile = nullptr;
-  fopen_s(&vbFile, vbufferpath.c_str(), "rb");
-  int e = 0;
-  int c;
-  while ((c = fgetc(vbFile)) != EOF)
+	std::string vbufferpath;
+	std::string outputFile;
+
+	std::cout << "Vertex Buffer path: ";
+	std::cin >> vbufferpath;
+	std::cout << "Output File Name: ";
+	std::cin >> outputFile;
+
+	FILE* vbFile = nullptr;
+	fopen_s(&vbFile, vbufferpath.c_str(), "rb");
+	int curpos = ftell(vbFile);
+	int e = 0;
+	int c;
+	while ((c = fgetc(vbFile)) != EOF)
 	{
-		
+
 		if (e < 1)
 			fseek(vbFile, 0, SEEK_SET);
-      
+
 		int16_t ix;
 		int16_t iy;
 		int16_t iz;
@@ -49,19 +54,32 @@ int main()
 		fread((char*)&ix, 1, 2, vbFile);
 		fread((char*)&iy, 1, 2, vbFile);
 		fread((char*)&iz, 1, 2, vbFile);
-		//divide by int16 max to get vertex position as float value
 
-		float xPos1((float)(ix / 32767));
-		float yPos1((float)(iy / 32767));
-		float zPos1((float)(iz / 32767));
+		float xPos1((float)ix);
+		float yPos1((float)iy);
+		float zPos1((float)iz);
+
+		//leftover, scale is found in main model at 0x3C
+
+		int scaleX = 1;
+		int scaleY = 1;
+		int scaleZ = 1;
+	
+		//divide by int16 max to get vertex position as float value
+		double xPos((xPos1 / 32767) * scaleX);
+		double yPos((yPos1 / 32767) * scaleY);
+		double zPos((zPos1 / 32767) * scaleZ);
 
 		std::string outXYZ;
-		outXYZW = "v " + to_str(xPos, 6) + " " + to_str(yPos, 6) + " " + to_str(zPos, 6) + "\n";		
-		filePutContents((outputFile + ".obj"), outXYZW, true);
-    
+		outXYZ = "v " + to_str(xPos, 6) + " " + to_str(yPos, 6) + " " + to_str(zPos, 6) + "\n";
+
+		curpos = ftell(vbFile);
+
+		std::string outfile = outputFile + ".obj";
+		filePutContents(outfile, outXYZ, true);
 		int curoff = ftell(vbFile);
 		fseek(vbFile, (curoff + 9), SEEK_SET);
-		
+
 		e += 1;
 	}
 }
