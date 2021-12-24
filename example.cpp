555f4a071550dfee5470474e551d98fd2a207679
nblockbuster <34419030+nblockbuster@users.noreply.github.com>
2021-12-24 13:09:49 -0500

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,73 @@
+// This isn't a full working example, mostly because I'm writing this while at a christmas party.
+// (basically find what imports are needed for this to work lmao)
+// This also only shows how to parse a vertex buffer.
+
+//ease of use cus why not
+void filePutContents(const std::string& name, const std::string& content, bool append = false) {
+	std::ofstream outfile;
+	if (append)
+		outfile.open(name, std::ios_base::app);
+	else
+		outfile.open(name);
+	outfile << content;
+}
+//again, ease of use
+
+template <typename T>
+std::string to_str(const T a_value, const int n = 6)
+{
+	double a = a_value;
+	std::stringstream out;
+	out << std::fixed << std::setprecision(n) << a;
+	return out.str();
+}
+
+int main()
+{
+  std::string vbufferpath;
+  std::string outputFile;
+  
+  std::cout << "Vertex Buffer path: ";
+  std::cin >> vbufferpath;
+  std::cout << "Output File Name: ";
+  std::cin >> outputFile;
+  
+  std::ifstream vbf;
+  vbf.open(vbufferpath.c_str(), std::ios_base::binary);
+  vbf.seekg(0, vbf.end);
+  int vbsize = vbf.tellg();
+  vbf.close();
+  
+  FILE* vbFile = nullptr;
+  fopen_s(&vbFile, vbufferpath.c_str(), "rb");
+  int e = 0;
+  int c;
+  while ((c = fgetc(vbFile)) != EOF)
+	{
+		
+		if (e < 1)
+			fseek(vbFile, 0, SEEK_SET);
+      
+		int16_t ix;
+		int16_t iy;
+		int16_t iz;
+
+		fread((char*)&ix, 1, 2, vbFile);
+		fread((char*)&iy, 1, 2, vbFile);
+		fread((char*)&iz, 1, 2, vbFile);
+		//divide by int16 max to get vertex position as float value
+
+		float xPos1((float)(ix / 32767));
+		float yPos1((float)(iy / 32767));
+		float zPos1((float)(iz / 32767));
+
+		std::string outXYZ;
+		outXYZW = "v " + to_str(xPos, 6) + " " + to_str(yPos, 6) + " " + to_str(zPos, 6) + "\n";		
+		filePutContents((outputFile + ".obj"), outXYZW, true);
+    
+		int curoff = ftell(vbFile);
+		fseek(vbFile, (curoff + 9), SEEK_SET);
+		
+		e += 1;
+	}
+}
